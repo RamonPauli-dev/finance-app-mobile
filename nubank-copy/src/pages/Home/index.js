5e9c32a1c962c2fef4bab1428ce72985a6fb0546
RamonPauli-dev <rp.ramonpauli@gmail.com>
2023-03-29 23:32:18 -0300

    balance

--- nubank-copy/src/pages/Home/index.js
+++ nubank-copy/src/pages/Home/index.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from '../../components/Header';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Balance from "../../components/Balance";
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        <Header name="ramon pauli"/>
-      <Text>pagina home</Text>
-      <StatusBar style="auto" />
+      <Header name="ramon pauli" />
+      <Balance balance={873.99} expenses={49.87}/>
     </View>
   );
 }
@@ -15,6 +15,6 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fafafa",
   },
 });
