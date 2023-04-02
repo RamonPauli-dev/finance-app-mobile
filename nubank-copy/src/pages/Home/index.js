8a4891ff7cadff2a951253f4bf8ffa71953c9078
RamonPauli-dev <rp.ramonpauli@gmail.com>
2023-04-02 19:50:22 -0300

    create movements
    lista com itens criada e adicionada na Home

--- nubank-copy/src/pages/Home/index.js
+++ nubank-copy/src/pages/Home/index.js
@@ -1,13 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Balance from "../../components/Balance";
 import Header from "../../components/Header";
+import Movements from "../../components/Movements";
 
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta de luz',
+    value: '350,47',
+    date: '12/07/2023',
+    type: 0 //despesa
+  },
+  {
+    id: 2,
+    label: 'mercado',
+    value: '487,55',
+    date: '15/07/2023',
+    type: 0 //despesa
+  },
+  {
+    id: 3,
+    label: 'transação via PIX',
+    value: '850,00',
+    date: '16/07/2023',
+    type: 1 //receita
+  },
+]
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="ramon pauli" />
       <Balance balance={873.99} expenses={49.87}/>
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList 
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <Movements data={item}/>}
+      />
     </View>
   );
 }
@@ -17,4 +50,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fafafa",
   },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14
+  }
 });
